4256c201ad81922eec80b41953d572cb91bf5d14
Srdjan Micin <smicin@domain.local>
2018-07-09 16:45:30 +0200

    _____

--- realization/HbbTV_Test/HbbTV_Test.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xsim HbbTV_Test_tb_behav -key {Behavioral:sim_1:Functional:HbbTV_Test_tb} -tclbatch HbbTV_Test_tb.tcl -view D:/DiplomskiHbbTV/sim/BeepTest_tb_behav.wcfg -view D:/DiplomskiHbbTV/sim/FlashTest_tb_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
